0679b838c8587c809fb4f95f9fd761d1dd5e4a64
hasegawagota <gota.hasegawa@kinto-technologies.com>
2023-07-27 09:31:00 +0900

    echo check

--- .github/shared/extract_pr_titles_and_links.sh
+++ .github/shared/extract_pr_titles_and_links.sh
@@ -2,6 +2,7 @@
 
 # コミット履歴からPRのタイトルとリンクを抽出する関数
 extract_pr_titles_and_links() {
+    echo "extract_pr_titles_and_links"
     git log --pretty=format:'%s' | \
     while read -r line; do
         title=$(echo "$line" | sed -n 's/\(.*\) (#.*/\1/p')
