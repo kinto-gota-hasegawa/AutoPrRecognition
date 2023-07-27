8c4a7bce6dac6902d13a44ddb2049cb96500aadc
hasegawagota <gota.hasegawa@kinto-technologies.com>
2023-07-27 09:14:28 +0900

    create github action

---
+++ .github/shared/extract_pr_titles_and_links.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# コミット履歴からPRのタイトルとリンクを抽出する関数
+extract_pr_titles_and_links() {
+    git log --pretty=format:'%s' | \
+    while read -r line; do
+        title=$(echo "$line" | sed -n 's/\(.*\) (#.*/\1/p')
+        pr_number=$(echo "$line" | sed -n 's/.*(#\(.*\))/\1/p')
+        echo "$title"
+        echo "> https://github.com/kinto-gota-hasegawa/AutoPrRecognition/pull/$pr_number"
+    done
+}
+
+extract_pr_titles_and_links
